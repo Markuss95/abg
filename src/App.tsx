d83565769e5534f35e5fcb94a7e9f15be24933c1
Markuss95 <51216200+Markuss95@users.noreply.github.com>
2022-10-04 14:08:39 +0200

    hexagon correction

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
     <Wrapper>
       <header>
         <p>Edit and save to reload.</p>
-        <div className="hexagon"></div>
+        <div className="hexagon">
+          <div className="first-icon">Ikona</div>
+        </div>
       </header>
     </Wrapper>
   );
@@ -15,12 +17,16 @@ const Wrapper = styled.div`
   font-family: "Regular";
   .hexagon {
     position: relative;
+    cursor: pointer;
     margin: 1em auto;
-    width: 10em;
-    height: 17.32em;
+    width: 7em;
+    height: 11.5em;
     border-radius: 1em/0.5em;
-    background: orange;
+    background: #708090;
     transition: opacity 0.5s;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
   .hexagon:before,
   .hexagon:after {
@@ -40,6 +46,10 @@ const Wrapper = styled.div`
   .hexagon:after {
     -webkit-transform: rotate(-60deg);
     transform: rotate(-60deg);
+  }
+  .first-icon {
+    position: absolute;
+    z-index: 1;
   }
 `;
 export default App;
