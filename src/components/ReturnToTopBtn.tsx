193c814d324b49c17ab732a7bc9386fcde32f09e
Markuss95 <51216200+Markuss95@users.noreply.github.com>
2022-10-11 11:47:19 +0200

    Removed unnecesary components

--- src/components/ReturnToTopBtn.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
-const ReturnToTopBtn = () => {
-  return <Wrapper></Wrapper>;
-};
-
-const Wrapper = styled.div``;
-export default ReturnToTopBtn;
