cb3c3d2ae27373eceaec0f26f80d7dbdf2bd87a7
Markuss95 <51216200+Markuss95@users.noreply.github.com>
2022-10-27 13:49:28 +0200

    Triggering netlify build

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import Footer from "../components/Footer";
 import backgroundPhoto from "../assets/Usluge.jpg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRoad, faCube, faFlaskVial } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faRoad, faCube, faFlaskVial } from "@fortawesome/free-solid-svg-icons";
 
 const ServicesPage = () => {
   return (
