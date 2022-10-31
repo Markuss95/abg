b9a05b99d810e10d39901fe9dfedd4f80199e5d2
Markuss95 <51216200+Markuss95@users.noreply.github.com>
2022-10-31 13:12:06 +0100

    Working on adding google maps to app

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+// import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import Footer from "../components/Footer";
 import contactsPhoto from "../assets/asd.jpg";
 const ContactsPage = () => {
   return (
     <>
-      <Wrapper>
+      <ContactsWrapper>
         <div className="image-container">
           <img
             src={contactsPhoto}
@@ -12,19 +13,30 @@ const ContactsPage = () => {
             className="homeImage"
           />
         </div>
-      </Wrapper>
+        <div className="contacts-container">
+          <p>{process.env.NODE_ENV}</p>
+          <p>{process.env.REACT_APP_NOT_SECRET_CODE}</p>
+          <p>test</p>
+          {/* <Wrapper></Wrapper> */}
+        </div>
+      </ContactsWrapper>
       <Footer />
     </>
   );
 };
 
-const Wrapper = styled.div`
+const ContactsWrapper = styled.div`
+  .contacts-container {
+    height: 60vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    background-color: #d3d3d3;
+  }
   .image-container {
     position: relative;
     width: 100vw;
-    height: 100vh;
+    height: 60vw;
     overflow: hidden;
-
     img {
       min-width: 100vw;
       object-fit: cover;
