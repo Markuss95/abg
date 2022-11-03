cdcac1e6ac4a8f93c0e70f03c8a6420f1e28a290
Markuss95 <51216200+Markuss95@users.noreply.github.com>
2022-11-03 12:13:49 +0100

    First version of about us photo

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,7 +1,34 @@
 import styled from "styled-components";
-
+import Footer from "../components/Footer";
+import AboutUsPhoto from "../assets/aboutus.jpg";
 const AboutUsPage = () => {
-  return <div>AboutUsPage</div>;
+  return (
+    <>
+      <Wrapper>
+        <div className="image-container">
+          <img
+            src={AboutUsPhoto}
+            alt="backgroundPhoto"
+            className="aboutImage"
+          />
+        </div>
+      </Wrapper>
+      <Footer />
+    </>
+  );
 };
+
+const Wrapper = styled.div`
+  .image-container {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    img {
+      min-width: 100vw;
+      object-fit: cover;
+    }
+  }
+`;
 
 export default AboutUsPage;
