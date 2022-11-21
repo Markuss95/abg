74bb64b54924cf4793c295a230470fe77a763289
Markuss95 <51216200+Markuss95@users.noreply.github.com>
2022-11-21 11:11:03 +0100

    about us page done

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -12,6 +12,20 @@ const AboutUsPage = () => {
             className="aboutImage"
           />
         </div>
+        <div className="about-us-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            repudiandae aperiam distinctio corrupti, tempore explicabo dolor
+            quos! Perspiciatis voluptate quis aperiam at officiis debitis,
+            eveniet odio quo excepturi unde incidunt!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste earum
+            mollitia, ea libero tempora neque labore. Facilis earum nemo
+            perferendis possimus rerum omnis. Enim molestias quaerat rem, facere
+            vitae voluptates?
+          </p>
+        </div>
       </Wrapper>
       <Footer />
     </>
@@ -19,6 +33,19 @@ const AboutUsPage = () => {
 };
 
 const Wrapper = styled.div`
+  .about-us-container {
+    position: relative;
+    height: 60vh;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(375px, 1fr));
+    color: white;
+    p {
+      text-align: left;
+      line-height: 3rem;
+      font-size: 1.4rem;
+      margin: 4rem 4rem;
+    }
+  }
   .image-container {
     position: relative;
     width: 100vw;
