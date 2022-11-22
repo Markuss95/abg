31e62d5f41166cfbe74ce22e3ddae58a15f650c0
Markuss95 <51216200+Markuss95@users.noreply.github.com>
2022-11-22 14:10:05 +0100

    Asphalt page begining setup

--- src/pages/AsphaltPage.tsx
+++ src/pages/AsphaltPage.tsx
@@ -1,9 +1,27 @@
 import styled from "styled-components";
-
+import infratestPhoto from "../assets/infratest.jpg";
 const AsphaltPage = () => {
-  return <Wrapper>AsphaltPage</Wrapper>;
+  return (
+    <Wrapper>
+      <div className="image-container">
+        <img
+          src={infratestPhoto}
+          alt="backgroundPhoto"
+          className="infratestImage"
+        />
+      </div>
+      <div>test</div>
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  .infratestImage {
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+    object-fit: cover;
+  }
+`;
 
 export default AsphaltPage;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,13 +3,6 @@ import Footer from "../components/Footer";
 import backgroundPhoto from "../assets/abgsl2.jpg";
 import homePhoto from "../assets/Testiranjeasfalta.jpg";
 const HomePage = () => {
-  // const [showFooter, setShowFooter] = useState<boolean>(true);
-  // useEffect(() => {
-  //   if (window.innerWidth < 943) {
-  //     setShowFooter(false);
-  //   }
-  // });
-
   return (
     <>
       <Wrapper>
