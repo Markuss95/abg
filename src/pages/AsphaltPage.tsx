7bd6cd928b62ed8ebb843b0ddd7aa98a07a24bd8
Markuss95 <51216200+Markuss95@users.noreply.github.com>
2022-11-23 10:20:53 +0100

    Asphalt table complete

--- src/pages/AsphaltPage.tsx
+++ src/pages/AsphaltPage.tsx
@@ -3,87 +3,77 @@ import { useMemo } from "react";
 import infratestPhoto from "../assets/infratest.jpg";
 import MaterialReactTable, { MRT_ColumnDef } from "material-react-table";
 
-type Person = {
-  name: {
-    firstName: string;
-    lastName: string;
-  };
-  address: string;
-  city: string;
-  state: string;
+type Testings = {
+  materials: string;
+  property: string;
+  test_method: string;
 };
 
 //nested data is ok, see accessorKeys in ColumnDef below
-const data: Person[] = [
+const data: Testings[] = [
   {
-    name: {
-      firstName: "John",
-      lastName: "Doe",
-    },
-    address: "261 Erdman Ford",
-    city: "East Daphne",
-    state: "Kentucky",
+    materials: "Bitumenske mješavine",
+    property: "Topivi udio veziva",
+    test_method: "HRN EN 12697-1:2020 (EN 12697-1:2020)",
   },
   {
-    name: {
-      firstName: "Jane",
-      lastName: "Doe",
-    },
-    address: "769 Dominic Grove",
-    city: "Columbus",
-    state: "Ohio",
+    materials: "Bitumenske mješavine",
+    property: "Određivanje granulometrijskog sastava",
+    test_method: "HRN EN 12697-2:2019 (EN 12697-2:2015 + A1:2019)",
   },
   {
-    name: {
-      firstName: "Joe",
-      lastName: "Doe",
-    },
-    address: "566 Brakus Inlet",
-    city: "South Linda",
-    state: "West Virginia",
+    materials: "Bitumenske mješavine",
+    property: "Određivanje gustoće asfaltne mješavine - otapalom",
+    test_method: "HRN EN 12697-5:2019 (EN 12697-5:2018) Postupak A",
   },
   {
-    name: {
-      firstName: "Kevin",
-      lastName: "Vandy",
-    },
-    address: "722 Emie Stream",
-    city: "Lincoln",
-    state: "Nebraska",
+    materials: "Bitumenske mješavine",
+    property: "Određivanje gustoće asfaltnih uzoraka",
+    test_method: "HRN EN 12697-6:2020 (EN 12697-6:2020) Točka 9.3 Postupak B",
   },
   {
-    name: {
-      firstName: "Joshua",
-      lastName: "Rolluffs",
-    },
-    address: "32188 Larkin Turnpike",
-    city: "Omaha",
-    state: "Nebraska",
+    materials: "Bitumenske mješavine",
+    property: "Određivanje šupljina u asfaltnim uzorcima",
+    test_method: "HRN EN 12697-8:2019 (EN 12697-8:2018) Točka 4",
+  },
+  {
+    materials: "Bitumenske mješavine",
+    property:
+      "Ispitne metode za asfalt proizveden vrućim postupkom - Uzorkovanje",
+    test_method: "HRN EN 12697-27:2017 (EN 12697-27:2017) Točke 4.1, 4.3, 4.7",
+  },
+  {
+    materials: "Bitumenske mješavine",
+    property:
+      "Priprema uzorka za određivanje udjela veziva, udjela vode i granulometrijskog sastava",
+    test_method: "HRN EN 12697-28:2020 (EN 12697-28:2020)",
+  },
+  {
+    materials: "Bitumenske mješavine",
+    property: "Priprema uzorka udarnim zbijačem",
+    test_method: "HRN EN 12697-30:2019 (EN 12697-30:2018) Izuzev točke 5.2",
+  },
+  {
+    materials: "Bitumen i bitumenska veziva",
+    property: "Određivanje točke razmekšanja postupkom prstena i kuglice",
+    test_method: "HRN EN 1427:2015 (EN 1427:2015)",
   },
 ];
 
 const AsphaltPage = () => {
-  const columns = useMemo<MRT_ColumnDef<Person>[]>(
+  const columns = useMemo<MRT_ColumnDef<Testings>[]>(
     () => [
       {
-        accessorKey: "name.firstName", //access nested data with dot notation
-        header: "First Name",
+        accessorKey: "materials", //access nested data with dot notation
+        header: "Materijali/Proizvodi",
       },
       {
-        accessorKey: "name.lastName",
-        header: "Last Name",
+        accessorKey: "property", //access nested data with dot notation
+        header: "Vrsta ispitivanja/Svojstvo",
       },
       {
-        accessorKey: "address", //normal accessorKey
-        header: "Address",
-      },
-      {
-        accessorKey: "city",
-        header: "City",
-      },
-      {
-        accessorKey: "state",
-        header: "State",
+        accessorKey: "test_method", //access nested data with dot notation
+        header: "Metoda ispitivanja",
       },
     ],
     []
@@ -95,7 +85,24 @@ const AsphaltPage = () => {
         alt="backgroundPhoto"
         className="infratestImage"
       />
-      <MaterialReactTable columns={columns} data={data} />
+      <MaterialReactTable
+        columns={columns}
+        data={data}
+        localization={{
+          clearSearch: "Očisti pretragu",
+          filterByColumn: "",
+          hideAll: "Sakrij sve",
+          of: "od",
+          rowsPerPage: "Redova po stranici",
+          showAll: "Prikaži sve",
+          showHideSearch: "Prikaži/sakrij pretragu",
+          showHideFilters: "Prikaži/sakrij filtere",
+          showHideColumns: "Prikaži/sakrij stupce",
+          search: "Pretraga",
+          toggleDensity: "Skupi redove",
+          toggleFullScreen: "Puni zaslon",
+        }}
+      />
     </Wrapper>
   );
 };
