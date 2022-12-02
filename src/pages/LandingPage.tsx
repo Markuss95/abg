a547562a326273372177035b69c025edf6ff5154
Markuss95 <51216200+Markuss95@users.noreply.github.com>
2022-12-02 12:54:01 +0100

    Trying to push to production

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import UpperHexagons from "../components/UpperHexagons";
 import { useState, useEffect, CSSProperties } from "react";
 import { useDispatch } from "react-redux";
